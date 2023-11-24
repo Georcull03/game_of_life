a258823c1b9c7918f0534982cbc67f6b14cec9c1
George Cullender <cullenge@amazon.co.uk>
2023-11-24 07:48:29 +0000

    Implemented functionality for evolve method which keeps track of neighbour cells to target cells then iterates through the deltas to see which cells stay alive and which cells come alive.

--- game_of_life/grid.py
+++ game_of_life/grid.py
@@ -1,9 +1,35 @@
+import collections
+
+
 class LifeGrid:
     def __init__(self, pattern):
         self.pattern = pattern
 
     def evolve(self):
-        pass
+        neighbours = {
+            (-1, -1),  # Above left
+            (-1, 0),  # Above
+            (-1, 1),  # Above right
+            (0, -1),  # Left
+            (0, 1),  # Right
+            (1, -1),  # Below left
+            (1, 0),  # Below
+            (1, 1),  # Below right
+        }
+
+        num_neightbours = collections.defaultdict(int)
+        for row, col in self.pattern.alive_cells:
+            for drow, dcol in neighbours:
+                num_neightbours[(row + drow, col + dcol)] += 1
+
+        stay_alive = {
+            cell for cell, num  in num_neightbours.items() if num in {2, 3}
+        } & self.pattern.alive_cells
+        come_alive = {
+            cell for cell, num in num_neightbours.items() if num == 3
+        } & self.pattern.alive_cells
+
+        self.pattern.alive_cells = stay_alive | come_alive
 
     def as_string(self, bbox):
         pass
