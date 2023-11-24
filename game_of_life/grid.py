6b0f73d462f505f4e6baeeb0757d1d198fe47e3c
George Cullender <cullenge@amazon.co.uk>
2023-11-24 10:09:49 +0000

    Defined DEAD and ALIVE constants which represent dead and live cells on the grid. Added functionality to as_string method - defines which part of the infinite grid display on screen.

--- game_of_life/grid.py
+++ game_of_life/grid.py
@@ -1,5 +1,8 @@
 import collections
 
+ALIVE = "❤"
+DEAD = "."
+
 
 class LifeGrid:
     def __init__(self, pattern):
@@ -23,16 +26,24 @@ class LifeGrid:
                 num_neightbours[(row + drow, col + dcol)] += 1
 
         stay_alive = {
-            cell for cell, num  in num_neightbours.items() if num in {2, 3}
-        } & self.pattern.alive_cells
+                         cell for cell, num in num_neightbours.items() if num in {2, 3}
+                     } & self.pattern.alive_cells
         come_alive = {
-            cell for cell, num in num_neightbours.items() if num == 3
-        } - self.pattern.alive_cells
+                         cell for cell, num in num_neightbours.items() if num == 3
+                     } - self.pattern.alive_cells
 
         self.pattern.alive_cells = stay_alive | come_alive
 
     def as_string(self, bbox):
-        pass
+        start_col, start_row, end_col, end_row = bbox
+        display = [self.pattern.name.center(2 * (end_col - start_col))]
+        for row in range(start_row, end_row):
+            display_row = [
+                ALIVE if (row, col) in self.pattern.alive_cells else DEAD
+                for col in range(start_col, end_col)
+            ]
+            display.append(" ".join(display_row))
+        return "\n ".join(display)
 
     def __str__(self):
         return (
